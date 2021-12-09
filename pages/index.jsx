0d92d48e60e787f440d9122899d5ba250b5c3857
Russell-Ali <resul.eliyev2000@mail.ru>
2021-12-10 00:03:56 +0400

    it's coming together

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,18 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
 export default function Home() {
-  return <dev></dev>;
+  return (
+    <dev className={styles.mainWrapper}>
+      <div className={styles.textBox_1}>
+        <div className={styles.line_1_1}>
+          <h1 className={styles.text_1_1}>hi,</h1>
+        </div>
+        <div className={styles.line_1_2}>
+          <h1 className={styles.text_1_2}>welcome to my webpage</h1>
+          <dev className={styles.cursorVisual}></dev>
+        </div>
+      </div>
+    </dev>
+  );
 }
