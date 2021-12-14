85fdda095d83331b50b0797789051e4939a6a5f2
Russell-Ali <resul.eliyev2000@mail.ru>
2021-12-14 23:48:43 +0400

    further improvements & starting of main layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,20 +5,26 @@ import Typing from "react-typing-animation";
 export default function Home() {
   const textBox = useRef();
   const mainContent = useRef();
+  const card_1 = useRef();
   const greeterFirst = () => {
     console.log("greeter first");
     gsap.to(textBox.current, {
       duration: "1",
       delay: "1.5",
-      y: "-=150px",
+      y: "-=8em",
       ease: "sine.out",
     });
     gsap.to(mainContent.current, {
       duration: "1",
       delay: "1.5",
-      y: "-=150px",
+      y: "-=8em",
       ease: "sine.out",
+      height: "+=8em",
     });
+  };
+
+  const cardFlip = () => {
+    console.log("Flipped");
   };
 
   return (
@@ -27,13 +33,15 @@ export default function Home() {
         ref={textBox}
         className={`${styles.textBox_1} ${0 ? styles.textBox_1_anim : null}`}
       >
+        <p id={styles.prompt} className={styles.text_1_1}>
+          $ echo
+        </p>
         <Typing
           className={styles.text_1_1}
           speed={150}
           startDelay={200}
           cursor={<div className={styles.cursorVisual}></div>}
           onFinishedTyping={() => {
-            console.log("just finished");
             greeterFirst();
           }}
         >
@@ -47,19 +55,10 @@ export default function Home() {
           <span> my name is Rasul btw</span>
         </Typing>
       </div>
-      <div
-        className={`${styles.mainContent} ${
-          0 ? styles.mainContent_anim : null
-        }`}
-      >
-        <p ref={mainContent} className={styles.content_1}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          officiis blanditiis odio deleniti hic delectus quae illum est eius aut
-          sapiente obcaecati repellendus modi magnam, optio eos nisi debitis
-          velit. Eius possimus quis fugit, at perferendis quia temporibus
-          voluptatum iure, reiciendis ducimus voluptas sapiente maxime itaque
-          nam non veniam debitis.
-        </p>
+      <div className={styles.mainContent} ref={mainContent}>
+        <div onClick={cardFlip} ref={card_1} className={styles.card_1}>
+          <p>Contact information</p>
+        </div>
       </div>
     </dev>
   );
